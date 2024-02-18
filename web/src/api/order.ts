92c0423c6b76ce1118c8cfd5396d97e73bfff751
Tim <lovezww2011@gmail.com>
2024-02-18 09:24:52 +0800

    fix

---
+++ web/src/api/order.ts
@@ -0,0 +1,44 @@
+import {get, post} from '/@/utils/http/axios';
+
+enum URL {
+    list = '/api/order/list',
+    create = '/api/order/create',
+    update = '/api/order/update',
+    pass = '/api/order/passOrder',
+    delete = '/api/order/delete',
+    cancel = '/api/order/cancelOrder',
+    cancelUserOrder = '/api/order/cancelUserOrder',
+    userOrderList = '/api/order/userOrderList',
+}
+
+const listApi = async (params: any) =>
+    get<any>({url: URL.list, params: params, data: {}, headers: {}});
+const userOrderListApi = async (params: any) =>
+    get<any>({url: URL.userOrderList, params: params, data: {}, headers: {}});
+
+const createApi = async (data: any) =>
+    post<any>({
+        url: URL.create,
+        params: {},
+        data: data,
+        headers: {'Content-Type': 'multipart/form-data;charset=utf-8'}
+    });
+const updateApi = async (params: any, data: any) =>
+    post<any>({
+        url: URL.update,
+        params: params,
+        data: data,
+        headers: {'Content-Type': 'multipart/form-data;charset=utf-8'}
+    });
+const deleteApi = async (params: any) =>
+    post<any>({url: URL.delete, params: params, headers: {}});
+
+const cancelApi = async (params: any) =>
+    post<any>({url: URL.cancel, params: params, headers: {}});
+
+const passApi = async (params: any) =>
+    post<any>({url: URL.pass, params: params, headers: {}});
+const cancelUserOrderApi = async (params: any) =>
+    post<any>({url: URL.cancelUserOrder, params: params, headers: {}});
+
+export {listApi, userOrderListApi, createApi, updateApi, passApi, deleteApi, cancelApi, cancelUserOrderApi};
